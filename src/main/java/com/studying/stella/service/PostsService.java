e69186d3ade2804dceb32a03009d18faadd09282
yoonji <stellayoonz@sookmyung.ac.kr>
2022-11-15 23:51:27 +0900

    section 2 commit _1

--- src/main/java/com/studying/stella/service/PostsService.java
+++ src/main/java/com/studying/stella/service/PostsService.java
@@ -3,13 +3,16 @@ package com.studying.stella.service;
 
 import com.studying.stella.domain.posts.PostRepository;
 import com.studying.stella.domain.posts.Posts;
+import com.studying.stella.web.dto.PostsListResponseDto;
 import com.studying.stella.web.dto.PostsResponseDto;
 import com.studying.stella.web.dto.PostsSaveRequestDto;
 import com.studying.stella.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +40,14 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+
+    }
+
 
 }
---
+++ src/main/java/com/studying/stella/web/IndexController.java
@@ -0,0 +1,31 @@
+package com.studying.stella.web;
+
+
+import com.studying.stella.service.PostsService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "post-save";
+    }
+
+}
+
+
